148eafd0e9d96a978e21729bc663027def8798bf
mananahujaa <63778460+mananahujaa@users.noreply.github.com>
2023-02-06 23:48:37 -0500

    Updated PP0
    Changed Linked list names

--- PP0.cpp
+++ PP0.cpp
@@ -25,52 +25,52 @@ private:
 
 template <typename E>
 
-class SNode { // singly linked list node
+class SCake { // singly linked list node
 private:
 E elem; // linked list element value
-SNode<E>* next; // next item in the list
-friend class SLinkedList<E>; // provide SLinkedList access
+SCake<E>* next; // next item in the list
+friend class CakeLinkedList<E>; // provide SLinkedList access
 };
 
 template <typename E>
-class SLinkedList { // a singly linked list
+class CakeLinkedList{ // a singly linked list
 public:
-SLinkedList(); // empty list constructor
-~SLinkedList(); // destructor
+CakeLinkedList(); // empty list constructor
+~CakeLinkedList(); // destructor
 bool empty() const; // is list empty?
 const E& front() const; // return front element
 void addFront(const E& e); // add to front of list
 void removeFront();       // remove front item list
 private:
-SNode<E>* head; // head of the list
+SCake<E>* head; // head of the list
 };
 
 // constructor
 template <typename E>
-SLinkedList<E>::SLinkedList() : head(NULL) { }
+CakeLinkedList<E>::CakeLinkedList() : head(NULL) { }
 
 // is list empty?
 template <typename E>
-bool SLinkedList<E>::empty() const {
+bool CakeLinkedList<E>::empty() const {
 return head == NULL;
 }
 
 // return front element
 template <typename E>
-const E& SLinkedList<E>::front() const {
+const E& CakeLinkedList<E>::front() const {
 return head->elem;
 }
 
 // destructor
 template <typename E>
-SLinkedList<E>::~SLinkedList() {
+CakeLinkedList<E>::~CakeLinkedList() {
 while (!empty()) removeFront();
 }
 
 // add to front of list
 template <typename E>
-void SLinkedList<E>::addFront(const E& e) {
-SNode<E>* v = new SNode<E>; // create new node
+void CakeLinkedList<E>::addFront(const E& e) {
+SCake<E>* v = new SCake<E>; // create new node
 v->elem = e; // store data
 v->next = head; // head now follows v
 head = v; // v is now the head
@@ -78,8 +78,8 @@ head = v; // v is now the head
 
 // remove front item
 template <typename E>
-void SLinkedList<E>::removeFront() {
-SNode<E>* old = head; // save current head
+void CakeLinkedList<E>::removeFront() {
+SCake<E>* old = head; // save current head
 head = old->next; // skip over old head
 delete old; // delete the old head
 }
