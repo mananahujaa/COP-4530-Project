ef3d32fa5ac5d7df7fb3cfea3c622b4ec2f1ad5c
Arwin <63482587+arwin-dev@users.noreply.github.com>
2023-02-07 10:14:17 -0500

    fomatting fixes

--- PP0.cpp
+++ PP0.cpp
@@ -25,24 +25,26 @@ private:
 
 template <typename E>
 
-class SCake { // singly linked list node
-private:
-E elem; // linked list element value
-SCake<E>* next; // next item in the list
-friend class CakeLinkedList<E>; // provide SLinkedList access
+class SCake 
+{ // singly linked list node
+    private:
+    E elem; // linked list element value
+    SCake<E>* next; // next item in the list
+    friend class CakeLinkedList<E>; // provide SLinkedList access
 };
 
 template <typename E>
-class CakeLinkedList{ // a singly linked list
-public:
-CakeLinkedList(); // empty list constructor
-~CakeLinkedList(); // destructor
-bool empty() const; // is list empty?
-const E& front() const; // return front element
-void addFront(const E& e); // add to front of list
-void removeFront();       // remove front item list
-private:
-SCake<E>* head; // head of the list
+class CakeLinkedList
+{ // a singly linked list
+    public:
+    CakeLinkedList(); // empty list constructor
+    ~CakeLinkedList(); // destructor
+    bool empty() const; // is list empty?
+    const E& front() const; // return front element
+    void addFront(const E& e); // add to front of list
+    void removeFront();       // remove front item list
+    private:
+    SCake<E>* head; // head of the list
 };
 
 // constructor
@@ -51,37 +53,42 @@ CakeLinkedList<E>::CakeLinkedList() : head(NULL) { }
 
 // is list empty?
 template <typename E>
-bool CakeLinkedList<E>::empty() const {
-return head == NULL;
+bool CakeLinkedList<E>::empty() const 
+{
+    return head == NULL;
 }
 
 // return front element
 template <typename E>
-const E& CakeLinkedList<E>::front() const {
-return head->elem;
+const E& CakeLinkedList<E>::front() const 
+{
+    return head->elem;
 }
 
 // destructor
 template <typename E>
-CakeLinkedList<E>::~CakeLinkedList() {
-while (!empty()) removeFront();
+CakeLinkedList<E>::~CakeLinkedList() 
+{
+    while (!empty()) removeFront();
 }
 
 // add to front of list
 template <typename E>
-void CakeLinkedList<E>::addFront(const E& e) {
-SCake<E>* v = new SCake<E>; // create new node
-v->elem = e; // store data
-v->next = head; // head now follows v
-head = v; // v is now the head
+void CakeLinkedList<E>::addFront(const E& e) 
+{
+    SCake<E>* v = new SCake<E>; // create new node
+    v->elem = e; // store data
+    v->next = head; // head now follows v
+    head = v; // v is now the head
 }
 
 // remove front item
 template <typename E>
-void CakeLinkedList<E>::removeFront() {
-SCake<E>* old = head; // save current head
-head = old->next; // skip over old head
-delete old; // delete the old head
+void CakeLinkedList<E>::removeFront() 
+{
+    SCake<E>* old = head; // save current head
+    head = old->next; // skip over old head
+    delete old; // delete the old head
 }
 
 int main() {
